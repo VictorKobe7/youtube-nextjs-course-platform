3119313fceb66c965d980cb6d42b693e42679182
Victor Kobe <victorkobe7@gmail.com>
2025-04-04 10:37:47 -0300

    Adicionado componente start course

--- src/app/cursos/[id]/page.tsx
+++ src/app/cursos/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { CourseHeader } from "@/components/course-header/CourseHeader";
+import { StartCourse } from "@/components/StartCourse";
 import { Metadata } from "next";
 
 interface Props {
@@ -14,7 +15,8 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 export default function PageCourseDatail({ params }: Props) {
   return (
     <main className="mt-8 flex justify-center">
-      <div className="w-full min-[880px]:max-w-[880px]">
+      <div className="w-full min-[880px]:max-w-[880px] px-2 flex flex-col gap-4">
+        <StartCourse idClass="1" idCourse="1" imageUrl="https://i.ytimg.com/vi/SVepTuBK4V0/hqdefault.jpg" />
         <CourseHeader />
       </div>
     </main>
