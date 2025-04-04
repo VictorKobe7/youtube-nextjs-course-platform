62a1adbece38df635719bef185bbbca09203ca37
Victor Kobe <victorkobe7@gmail.com>
2025-04-04 11:12:15 -0300

    Adicionado botão flutuante no componente start course

--- src/app/cursos/[id]/page.tsx
+++ src/app/cursos/[id]/page.tsx
@@ -16,7 +16,7 @@ export default function PageCourseDatail({ params }: Props) {
   return (
     <main className="mt-8 flex justify-center">
       <div className="w-full min-[880px]:max-w-[880px] px-2 flex flex-col gap-4">
-        <StartCourse idClass="1" idCourse="1" imageUrl="https://i.ytimg.com/vi/SVepTuBK4V0/hqdefault.jpg" />
+        <StartCourse title="title" idClass="1" idCourse="1" imageUrl="https://i.ytimg.com/vi/SVepTuBK4V0/hqdefault.jpg" />
         <CourseHeader />
       </div>
     </main>
