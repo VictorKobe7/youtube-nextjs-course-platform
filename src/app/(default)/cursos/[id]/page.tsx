73061ad1fa81651bc4330b88b9d74fa76213aaf8
Victor Kobe <victorkobe7@gmail.com>
2025-04-09 15:34:14 -0300

    Adicionado getById do youtube

--- src/app/(default)/cursos/[id]/page.tsx
+++ src/app/(default)/cursos/[id]/page.tsx
@@ -2,55 +2,59 @@ import { CourseContent } from "@/components/course-content/CourseContent";
 import { CourseHeader } from "@/components/course-header/CourseHeader";
 import { StartCourse } from "@/components/StartCourse";
 import { Metadata } from "next";
+import { APIYoutube } from "@/shared/services/api-youtube";
 
 interface Props {
   params: { id: string };
 }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const courseDetail = await APIYoutube.course.getById(params.id);
+
   return {
-    title: `CodarSe - ${params.id}`
+    title: courseDetail.title,
+    description: courseDetail.description,
+    openGraph: {
+      locale: "pt_BR",
+      type: "video.other",
+      title: courseDetail.title,
+      images: courseDetail.image,
+      description: courseDetail.description,
+      videos: courseDetail.classGroups
+        .reduce<string[]>((previous, current) => [
+          ...previous,
+          ...current.classes.map((classItem) => `https://www.codarse.com/player/${current.idCourse}/${classItem.id}`)
+        ], [])
+    }
   }
 }
 
-export default function PageCourseDatail({ params }: Props) {
+export default async function PageCourseDatail({ params }: Props) {
+  const courseDetail = await APIYoutube.course.getById(params.id);
+  const firstClass = courseDetail.classGroups.at(0)?.classes.at(0);
+
   return (
     <main className="mt-8 flex justify-center">
       <div className="w-full min-[880px]:max-w-[880px] px-2 flex flex-col gap-4 lg:px-0 md:flex-row-reverse">
-        <div className="flex-1">
-          <StartCourse
-            title="title"
-            idClass="1"
-            idCourse="1"
-            imageUrl="https://i.ytimg.com/vi/SVepTuBK4V0/hqdefault.jpg"
-          />
-        </div>
+        {firstClass && (
+          <div className="flex-1">
+            <StartCourse
+              title={firstClass.title}
+              idClass={firstClass.id}
+              idCourse={courseDetail.id}
+              imageUrl={courseDetail.image}
+            />
+          </div>
+        )}
 
         <div className="flex-[2] flex flex-col gap-12 pb-12">
           <CourseHeader
-            title="Title"
-            description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias amet fugiat illum. Ut dolorem natus optio, eos sunt enim? Quia explicabo soluta ipsam ab, eius eveniet quibusdam iure perferendis quos? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum odit non, consequuntur quisquam quos fuga! Dolores reiciendis, voluptatum molestias error eaque quo ut qui distinctio optio voluptates, nulla, minima culpa. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum magnam harum tempora quasi, corrupti voluptas exercitationem minus autem rerum id, veniam aliquid saepe neque reprehenderit, cupiditate unde molestiae ipsam distinctio?"
-            numberOfClasses={48}
+            title={courseDetail.title}
+            description={courseDetail.description}
+            numberOfClasses={courseDetail.numberOfClasses}
           />
 
-          <CourseContent
-            classGroups={[
-              {
-                title: "Introdução e Apresentação do Projeto",
-                idCourse: "1",
-                classes: [
-                  {
-                    id: "1",
-                    title: "Aula 1"
-                  },
-                  {
-                    id: "2",
-                    title: "Aula 2"
-                  },
-                ]
-              }
-            ]}
-          />
+          <CourseContent classGroups={courseDetail.classGroups} />
         </div>
       </div>
     </main>
